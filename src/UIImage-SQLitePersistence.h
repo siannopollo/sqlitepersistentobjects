cd6ab26a0b905432e1583c859357e328ab89825c
jeff.lamarche <jeff.lamarche@e86f48c2-4f55-0410-860d-1fbeb81bd84d>
2008-10-31 15:55:49 +0000

    Just updated the copyright line to reflect fact that additions by others are not (c) me. Everybody feel free to give yourself credit in the comments if you make changes or additions, I don't want to claim credit for your work.

--- src/UIImage-SQLitePersistence.h
+++ src/UIImage-SQLitePersistence.h
@@ -2,7 +2,7 @@
 // ----------------------------------------------------------------------
 // Part of the SQLite Persistent Objects for Cocoa and Cocoa Touch
 //
-// (c) 2008 Jeff LaMarche (jeff_Lamarche@mac.com)
+// Original Version: (c) 2008 Jeff LaMarche (jeff_Lamarche@mac.com)
 // ----------------------------------------------------------------------
 // This code may be used without restriction in any software, commercial,
 // free, or otherwise. There are no attribution requirements, and no
